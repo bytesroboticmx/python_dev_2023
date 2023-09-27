14a4494677fe9a85d212d8ee6ea0a324f6a8c751
Aldo Gonzalez <bytesrobotic@gmail.com>
2023-09-26 22:17:06 -0600

    Escribiendo comentarios iniciales del programa

---
+++ pro1.py
@@ -0,0 +1,5 @@
+#Programa que utiliza la biblioteca NLTK para realizar el análisis de texto simple.
+#Autor: Ph.D. Aldo Gonzàlez Vàzquez
+#Fecha: 20/08/2019
+#Versión: 1.0
+#Correo: bytesroboticmx@gmail.com
