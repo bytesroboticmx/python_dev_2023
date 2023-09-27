3ac76e04f58625fc7061c5ebb7457597b4d11960
bytesroboticmx <bytesroboticmx@gmail.com>
2023-09-27 08:29:58 -0600

    Cambio de la fecha inicial

--- pro1.py
+++ pro1.py
@@ -1,6 +1,6 @@
 #Programa que utiliza la biblioteca NLTK para realizar el análisis de texto simple.
 #Autor: Ph.D. Aldo Gonzàlez Vàzquez
-#Fecha: 20/08/2019
+#Fecha: 26/09/2023
 #Versión: 1.0
 #Correo: bytesroboticmx@gmail.com
 import nltk
---
+++ pro2.py
@@ -0,0 +1,3 @@
+#Programa en python que cuenta cuanta palabras tiene la oracion.
+#Autor: Ph.D. Aldo Gonzalez Vazquez
+#Fecha: 
